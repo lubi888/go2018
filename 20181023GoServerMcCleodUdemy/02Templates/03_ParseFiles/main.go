74b81d75159fa9de27e3881bdb9b99a3d3acfa57
xpsIveagh <linuxubiquitous@gmail.com>
2018-10-24 13:33:43 +0100

    templating - parsing data files

---
+++ 20181023GoServerMcCleodUdemy/02Templates/03_ParseFiles/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl, err := template.ParseFiles( "one.gmao")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil,)
+	if err != nil {
+		log.Fatalln(err)
+	}else {
+		fmt.Println()
+	}
+
+	tpl, err = tpl.ParseFiles("two.gmao", "vespa.gmao")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "vespa.gmao", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gmao", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "one.gmao", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ 20181023GoServerMcCleodUdemy/02Templates/06PassingData/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+"log"
+"os"
+"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("tpl.gohtml"))
+}
+
+func main() {
+	//err := tpl.Execute(os.Stdout, 42)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+
+
+	//err:= tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 42)
+	err:= tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", "release self focus, embracce other focus, etc")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
